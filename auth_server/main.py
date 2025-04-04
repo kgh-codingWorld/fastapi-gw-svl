ab461491a6bf55adbd21dbfc3b95e06b7d53d261
kgh-codingWorld <tto4028@naver.com>
2025-04-04 15:05:35 +0900

    change to lifespan from on_event, organize folders

--- auth_server/main.py
+++ auth_server/main.py
@@ -2,21 +2,30 @@ from fastapi import FastAPI
 from auth_server.routes import router
 import asyncpg
 import asyncio
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-app.include_router(router)
-
-@app.on_event("startup") #lifespan 
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("lifespan 진입 - DB 연결 시도 중...")
     for i in range(10):
         try:
             app.state.pool = await asyncpg.create_pool(
                 user="postgres", password="password",
                 database="auth", host="postgres"
             )
-            return
+            print("🎉 DB 연결 성공")
+            break
         except Exception as e:
             print(f"DB 연결 실패 {i+1}/10회 - {e}")
             await asyncio.sleep(1)
     else:
-        raise RuntimeError("DB 연결에 반복적으로 실패함")
+        raise RuntimeError("DB 연결에 반복적으로 실패함 ㅜ")
+
+    yield  # 앱이 실행되는 동안
+
+    print("앱 종료 - DB 커넥션 풀 정리 중...")
+    await app.state.pool.close()
+    print("DB 연결 종료 완료")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
--- gateway/core/router.py
+++ gateway/core/router.py
@@ -16,13 +16,18 @@ async def gateway(path: str, request: Request):
     if not api_key or not await is_valid_api_key(api_key):
         return JSONResponse(status_code=401, content={"error": "Unauthorized"})
 
-    target_url = ROUTES.get(f"/{path}")
-    if not target_url:
+    full_path = f"/{path}"
+    target_base_url = ROUTES.get(full_path)
+    if not target_base_url:
         return JSONResponse(status_code=404, content={"error": "Route not found"})
+
+    forward_url = f"{target_base_url}{full_path}"
 
     async with httpx.AsyncClient() as client:
         response = await client.request(
             method=request.method,
-            url=target_url,
+            url=forward_url,
+            headers=request.headers.raw,
+            content=await request.body()
         )
         return JSONResponse(status_code=response.status_code, content=response.json())
